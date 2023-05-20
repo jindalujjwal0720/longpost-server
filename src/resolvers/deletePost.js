838069a31eb4e3cf52a73e2499dfd30dd954c661
Ujjwal Jindal <jindalujjwal0720@gmail.com>
2023-05-20 13:31:08 +0530

    edit - delete - user

--- src/resolvers/deletePost.js
+++ src/resolvers/deletePost.js
@@ -2,13 +2,29 @@ const Post = require("../models/Post");
 
 const deletePostResolver = async (req, res) => {
   const id = req.params.postID;
+  const { userID } = req.body;
+  if (!userID) {
+    return res.status(400).json({
+      message: "User ID is required!",
+    });
+  }
   if (!id) {
     return res.status(400).json({
       message: "Post ID is required!",
     });
   }
   try {
-    const post = await Post.findByIdAndDelete(id);
+    const post = await Post.findById(id);
+    if (!post) {
+      return res.status(400).json({
+        message: "Post does not exist!",
+      });
+    } else if (post.userID !== userID) {
+      return res.status(400).json({
+        message: "User ID does not match!",
+      });
+    }
+    await post.delete();
     return res.status(200).json({
       post: post,
     });
--- src/resolvers/updatePost.js
+++ src/resolvers/updatePost.js
@@ -2,7 +2,7 @@ const Post = require("../models/Post");
 
 const updatePostResolver = async (req, res) => {
   const id = req.params.postID;
-  const { title, content } = req.body;
+  const { title, content, userID } = req.body;
   if (!id) {
     return res.status(400).json({
       message: "Post ID is required!",
@@ -13,16 +13,26 @@ const updatePostResolver = async (req, res) => {
       message: "Title and content are required!",
     });
   }
+  if (!userID) {
+    return res.status(400).json({
+      message: "User ID is required!",
+    });
+  }
   try {
-    const post = await Post.findByIdAndUpdate(
-      id,
-      {
-        title: title,
-        short: content.substring(0, 100),
-        content: content,
-      },
-      { new: true }
-    );
+    const post = await Post.findById(id);
+    if (!post) {
+      return res.status(400).json({
+        message: "Post does not exist!",
+      });
+    } else if (post.userID !== userID) {
+      return res.status(400).json({
+        message: "User ID does not match!",
+      });
+    }
+    post.title = title;
+    post.short = content.substring(0, 100);
+    post.content = content;
+    await post.save();
     return res.status(200).json({
       post: post,
     });
